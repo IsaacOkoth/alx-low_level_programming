19f8c957782509c4d162e0d160797737a3be180d
IsaacOkoth <isaacokoth2000@gmail.com>
2023-04-05 15:12:35 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_divisible - Entry point
+ * Describing:Prime number
+ * @n:Integer
+ * @div:integer
+ * Return:1 if it is a prime number otherwise
+ */
+
+int is_divisible(int n, int div)
+{
+	if (div == 1 || div == n)
+	{
+		return (0);
+	}
+	return (is_divisible(n, div - 1));
+}
+
+/**
+ * is_prime_number - Entry point
+ * Describing:prime number
+ * @n:integer
+ * Return:integer value
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (is_divisible(n, n - 1));
+}
